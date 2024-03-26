9f125e91d7c965aef4a9ca8168c138daf38d2cb6
Alessandro Pedroso <alessandropedrosoti@gmail.com>
2024-03-26 04:31:30 -0300

    primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+import Banner from './components/Banner.vue'
+import ConteudoPrincipal from './components/ConteudoPrincipal.vue';
+
+export default {
+  components: { Banner,ConteudoPrincipal }
+}
+// export default {
+//   components: {Banner:Banner}
+// }
+</script>
+
+<template>
+  <Banner />
+  <ConteudoPrincipal />
+  <!-- <h1 class="titulo">
+    Texto do app.vue
+  </h1> -->
+</template>
+
+<!-- <style scoped>
+.titulo{
+  color:red;
+}
+</style> -->
